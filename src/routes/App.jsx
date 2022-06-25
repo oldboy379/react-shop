ebb5d31af3f564bd1b9a2ead40d44071278f7b19
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-06-25 00:02:47 -0600

    [add] Main structure and Router

---
+++ src/routes/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../containers/Layout";
+import Home from "../pages/Home";
+import Login from "../containers/Login";
+import RecoveryPassword from "../containers/RecoveryPassword";
+import NotFound from "../pages/NotFound";
+import "../styles/global.css";
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/login" element={<Login />} />
+					<Route
+						exact
+						path="/recovery-password"
+						element={<RecoveryPassword />}
+					/>
+					<Route element={NotFound} />
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	);
+};
+
+export default App;
