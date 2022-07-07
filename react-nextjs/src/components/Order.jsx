fddd68f38e02bc0e266b6db62137222621c80940
Olman Francisco Castro Hernández <kstro379@gmail.com>
2022-07-07 10:17:32 -0600

    [update] Unused files

--- react-nextjs/src/components/Order.jsx
+++ react-nextjs/src/components/Order.jsx
@@ -1,19 +1,20 @@
-import React from "react";
-import "@styles/Order.scss";
+import React from 'react';
+import Image from 'next/image';
+import '@styles/Order.scss';
 
-import arrow from "@icons/flechita.svg";
+import arrow from '@icons/flechita.svg';
 
 const Order = () => {
-	return (
-		<div className="Order">
-			<p>
-				<span>03.25.21</span>
-				<span>6 articles</span>
-			</p>
-			<p>$560.00</p>
-			<img src={arrow} alt="arrow" />
-		</div>
-	);
+  return (
+    <div className="Order">
+      <p>
+        <span>03.25.21</span>
+        <span>6 articles</span>
+      </p>
+      <p>$560.00</p>
+      <Image src={arrow} alt="arrow" />
+    </div>
+  );
 };
 
 export default Order;
--- react-nextjs/src/templates/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NotFound = () => {
-	return <div>Not Found, 404 error</div>;
-};
-
-export default NotFound;
